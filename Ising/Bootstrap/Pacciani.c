4387c3bbef2e6eed3982707134df9295f3286946
Lorenzo Benfatto <59019419+Lorenzo-Benfatto@users.noreply.github.com>
2022-10-25 10:55:39 +0200

    Create Pacciani.c

---
+++ Ising/Bootstrap/Pacciani.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+  printf("Se ni’ mondo esistesse un po’ di bene \n e ognun si honsiderasse suo fratello \n ci sarebbe meno pensieri e meno pene \n e il mondo ne sarebbe assai più bello");
+  return 0;
+}
