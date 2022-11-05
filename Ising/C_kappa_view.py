29737758f4ec4e40c17d83f870b1f6895b67484f
Dario Rossi <115357035+dariorossi1712@users.noreply.github.com>
2022-11-05 12:57:05 +0100

    Add files via upload

---
+++ Ising/C_kappa_view.py
@@ -0,0 +1,46 @@
+# A program to plot the C(k) for different beta values for a given lattice lenght
+
+import pylab
+import numpy as np
+from datetime import datetime
+import matplotlib.pyplot as plt
+import os
+
+from datetime import datetime
+
+startTime = datetime.now()
+
+# assign directory
+directory = r"\\wsl.localhost\Ubuntu\home\dario\Documents\Metodi\Modulo1\Ising\Bootstrap\Nlatt=20\CK"
+
+pylab.figure(1)
+pylab.title('MODELLO DI ISING 2D \n Errore Magnetizzazione bootstrap in funzione del numero di resampling')
+pylab.xlabel('Numero di resampling')
+pylab.ylabel(r'$\sigma$')
+
+# iterate over files in that directory
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        if f.find('txt')!=-1:
+            print(f)
+            # nella prima colonna ho il numero di k e nella seconda il c(k) corrispondente
+            n, ck =np.loadtxt(f, unpack=True)
+            inizio = f.find('eta)')
+            if(inizio!=-1):
+                inizio = inizio + 4
+                fine = inizio + 4
+
+            beta=float(f[inizio:fine])
+
+            pylab.errorbar(n, ck, linestyle = '--', marker ='.', label = fr'$\beta$ = {beta}')
+            print(f'beta = {beta}')
+            pylab.legend()
+            pylab.minorticks_on()
+
+pylab.show()
+
+
+
+
