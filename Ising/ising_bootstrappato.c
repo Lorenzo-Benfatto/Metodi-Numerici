5bd65bfa1cafdc1bc7fd5ea6a7f9e066ab2c7294
Dario Rossi <115357035+dariorossi1712@users.noreply.github.com>
2022-11-16 12:28:27 +0100

    Add files via upload

---
+++ Ising/ising_bootstrappato.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <errno.h>
+#include"/home/dario/Documents/Metodi/usefulstuff.h"
+#include"/home/dario/Documents/Metodi/ran2.h"
+#include"/home/dario/Documents/Metodi/listfunction.h"
+
+
+#define L 100
+
+void bootstrap(FILE *f){
+    FILE *m ,*e;
+    unsigned long s;
+    int i=0,j,k,N=10000,M=100,x,bin=4,taglio=1;         /*N = numero di misure, M = numero di resampling, 
+    x serve per scanfare i file, i e j indici ricorsivi, bin è il bin del bootstrap, 
+    taglio è per levare le misure non termalizzate*/
+    int V=N-taglio;
+    long int seed = 13;         // seed per il ran2()
+    float mag, ene, beta;         // risultati del simulatore_boot.c e valore del beta corrente
+    int iter;           // risultati del simulatore_boot.c
+    float *dati_mag = malloc(sizeof(long double) * (N-taglio));
+    float *dati_ene = malloc(sizeof(long double) * (N-taglio));
+    
+
+    x=fscanf(f, "%f  %f  %d  %f", &mag, &ene, &iter, &beta);
+    while (x!=EOF){
+            dati_mag[i]=mag;
+            dati_ene[i]=ene;
+            x=fscanf(f, "%f  %f  %d  %f", &mag, &ene, &iter, &beta);
+            i++;
+        }
+    /* Questo file va aperto quando sappiamo già il valore di beta*/
+    char filemisure[500];
+    sprintf(filemisure, "/home/dario/Documents/Metodi/Modulo1/Ising/Bootstrap/Nlatt=%d/Bootstrappo/Magnetizzazione/resample_binned%.3f.txt", L, beta);
+    m=fopen(filemisure,"w");
+    control_file(m);
+    
+    sprintf(filemisure, "/home/dario/Documents/Metodi/Modulo1/Ising/Bootstrap/Nlatt=%d/Bootstrappo/Energia/resample_binned%.3f.txt", L, beta);
+    e=fopen(filemisure,"w");
+    control_file(e);
+    
+    for(j=0;j<M;j++){
+        for (k=taglio;k<N/(bin+1);k++){
+            s = ran2(&seed) * RAND_MAX;
+            s = (int)s;
+            s <<= 15;
+            s ^= (int)(ran2(&seed) * RAND_MAX);
+            s %= V+1;
+            s=s+taglio;
+            //printf("%ld \n",s);
+            for(int h=0;h<bin+1;h++){
+                if(s+h>N-1){
+                    fprintf(m,"%lf ",dati_mag[s+h+1-N]);
+                    fprintf(e,"%lf ",dati_ene[s+h+1-N]);
+                 //printf("sono dentro il primo if \n");
+                }
+                if(s+h<=N-1){
+                    fprintf(m,"%lf ",dati_mag[s+h]);
+                    fprintf(e,"%lf ",dati_ene[s+h]);
+                 //printf("sono dentro il secondo if \n");
+                }
+
+            }
+        }
+        fprintf(m,"\n");
+        fprintf(e,"\n");
+    }
+
+    free(dati_mag);
+    free(dati_ene);
+    fclose(f);
+    fclose(m);
+    fclose(e);
+    return;
+}
+
+
+int main()
+{
+    DIR *folder;
+    FILE *input;
+    struct dirent *entry;
+    int files = 0;
+    
+    char directory[500];
+    sprintf(directory, "/home/dario/Documents/Metodi/Modulo1/Ising/Bootstrap/Nlatt=%d/Risultati", L);
+    folder = opendir(directory);
+    if(folder == NULL)
+    {
+        perror("Oh Fra guarda che in Risultati non ci entro \n");
+        return(1);
+    }
+
+    while( (entry=readdir(folder)) )
+    {
+        files++;
+        
+        printf("File %3d: %s\n", files, entry->d_name);
+
+        if (!strcmp (entry->d_name, "."))
+            continue;
+        if (!strcmp (entry->d_name, ".."))
+            continue;
+            
+      
+        char path[500];
+        sprintf(path, "/home/dario/Documents/Metodi/Modulo1/Ising/Bootstrap/Nlatt=%d/Risultati/%s", L, entry->d_name); //it modifies each time the name of the file to be created
+        printf("sto con lei che sta fopenando \n");
+        input = fopen(path, "r");
+        printf("Io e i miei fra odiamo i puntatori \n");
+        if (input == NULL){
+            
+            fprintf(stderr, "Error : NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO\n");
+            exit(-1);
+        }
+        printf("sto per strappare gli scarponi\n");
+        bootstrap(input);
+    }
+
+    closedir(folder);
+
+    return(0);
+}
